0ea6924f6ee006e7f2954dba48d43ef0284c3c5d
Kelems <70765348+Kelems@users.noreply.github.com>
2021-12-05 19:27:54 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+//create a server object:
+http
+  .createServer(function (req, res) {
+    const date = new Date();
+
+    res.write("Hello ");
+    res.write("");
+    res.write(", today is ");
+    res.write(
+      `${new Intl.DateTimeFormat("en-EN", { weekday: "long" }).format(date)}`
+    );
+    res.end();
+  })
+  .listen(8080); //the server object listens on port 8080
